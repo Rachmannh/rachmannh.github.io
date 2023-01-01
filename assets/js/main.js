a98651339bc8021e52572802e3e1a32c5e564d37
Rachman Nul Hakim <rachmannul1999@gmail.com>
2023-01-02 04:59:02 +0700

    mengubah css

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,3 @@
-// Bootstrap Offcanvas
-const offcanvasElementList = document.querySelectorAll(".offcanvas");
-const offcanvasList = [...offcanvasElementList].map(
-  (offcanvasEl) => new bootstrap.Offcanvas(offcanvasEl)
-);
-
 // Nav Slide Animation
 const burgerMenu = document.querySelector(".burger-menu input");
 const nav = document.querySelector("nav ul");
