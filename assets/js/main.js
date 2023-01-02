215f2b069acde10ed33134b38b7834ffbe6d1a98
Rachman Nul Hakim <rachmannul1999@gmail.com>
2023-01-02 23:20:25 +0700

    membuat navbar

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,6 @@
-// Nav Slide Animation
-const burgerMenu = document.querySelector(".burger-menu input");
+const toggle = document.querySelector(".burger-menu input");
 const nav = document.querySelector("nav ul");
 
-burgerMenu.addEventListener("click", function () {
+toggle.addEventListener("click", function () {
   nav.classList.toggle("slide");
 });
