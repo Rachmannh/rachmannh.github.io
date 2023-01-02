43100849e31faf7ce709a8cd66ab63b26d6c522c
Rachman Nul Hakim <rachmannul1999@gmail.com>
2023-01-03 00:33:52 +0700

    membuat hero section

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,11 @@
-const toggle = document.querySelector(".burger-menu input");
-const nav = document.querySelector("nav ul");
+function scrollValue() {
+  var navbar = document.getElementById("navbar");
+  var scroll = window.scrollY;
+  if (scroll < 50) {
+    navbar.classList.remove("my-bg-custom");
+  } else {
+    navbar.classList.add("my-bg-custom");
+  }
+}
 
-toggle.addEventListener("click", function () {
-  nav.classList.toggle("slide");
-});
+window.addEventListener("scroll", scrollValue);
