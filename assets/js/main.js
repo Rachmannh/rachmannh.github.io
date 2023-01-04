8124deb57604f5c6489e5897a4c13923b8a5e7c4
Rachman Nul Hakim <rachmannul1999@gmail.com>
2023-01-04 22:47:48 +0700

    add changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,16 +5,3 @@ const nav = document.querySelector("nav ul");
 burgerToggle.addEventListener("click", function () {
   nav.classList.toggle("slide");
 });
-
-// Change Navbar BG Color on Hover
-function scrollValue() {
-  var navbar = document.getElementById("navbar");
-  var scroll = window.scrollY;
-  if (scroll < 50) {
-    navbar.classList.remove("my-bg-custom");
-  } else {
-    navbar.classList.add("my-bg-custom");
-  }
-}
-
-window.addEventListener("scroll", scrollValue);
